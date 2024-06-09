bf23460a6125a721e236f5fcf53f960912ca2cc5
Mohammed Hussain <hmohammed448@gmail.com>
2024-06-09 22:49:38 +0530

    all

--- src/Common/Components/FoodCard.jsx
+++ src/Common/Components/FoodCard.jsx
@@ -9,7 +9,7 @@ export default function FoodCard(props) {
     <>
       <div
         key={mealId}
-        className="w-full max-w-sm bg-white border border-gray-200 rounded-2xl shadow dark:bg-white dark:border-gray-300"
+        className="w-full relative max-w-sm bg-white border border-gray-200 rounded-2xl shadow dark:bg-white dark:border-gray-300"
       >
         <NavLink to={`viewItem/${mealId}/${Number(mealPrice)}`}>
           <img
@@ -75,12 +75,21 @@ export default function FoodCard(props) {
             </div>
           </div>
           <div className="flex items-center justify-between mt-2">
-            <span className="text-1xl font-bold text-gray-900 ">
-              code : #{mealId}
+            <span
+              className="text-xs absolute font-bold text-gray-900 px-2 py-1"
+              style={{
+                border: "none",
+                borderRadius: "0px 5px 5px 0",
+                top: "20px",
+                backgroundColor: "#ffa500",
+                left: "0",
+              }}
+            >
+              Food code : #{mealId}
             </span>
             <a
               href="#"
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className="text-white bg-red-700 hover:bg-red-800 focus:ring-0 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
             >
               Add to Cart
             </a>
